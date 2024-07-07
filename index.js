c40580096c1fd285bc41564f748deb9690abba2d
Awan mabur <82402071+Awanmabur@users.noreply.github.com>
2024-07-07 17:23:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const {superAdmin} = require('./seeders/admin');
 superAdmin();
 
 // *************************    Assets    ****************************************//
-app.use(urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
