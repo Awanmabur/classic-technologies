db6bc6617bcb8e9b60dce536c68cfeda88bad15a
Awan mabur <82402071+Awanmabur@users.noreply.github.com>
2024-07-07 15:16:29 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -12,70 +12,70 @@ router.get('/', checkLoginStatus, async (req, res) => {
 
   const blog = await BlogPost.find({}).limit(3);;
   const review = await Review.find({});
-  res.render("./home/index", { blog, user, review });
+  res.render("/home/index", { blog, user, review });
 });
 
 router.get('/contact', checkLoginStatus, async (req, res) => {
   const userId = req.user;
   const user = await User.findById(userId);
 
-  res.render("./home/contact", { user });
+  res.render("/home/contact", { user });
 });
 
 router.get('/top-management', checkLoginStatus, async (req, res) => {
   const userId = req.user;
   const user = await User.findById(userId);
 
-  res.render("./home/top-management", { user });
+  res.render("/home/top-management", { user });
 });
 
 router.get('/frontend', checkLoginStatus, async (req, res) => {
   const userId = req.user;
   const user = await User.findById(userId);
 
-  res.render("./home/frontends", { user });
+  res.render("/home/frontends", { user });
 });
 
 router.get('/backend', checkLoginStatus, async (req, res) => {
   const userId = req.user;
   const user = await User.findById(userId);
 
-  res.render("./home/backendprojects", { user });
+  res.render("/home/backendprojects", { user });
 });
 
 router.get('/graphics', checkLoginStatus, async (req, res) => {
   const userId = req.user;
   const user = await User.findById(userId);
 
-  res.render("./home/graphicprojects", { user });
+  res.render("/home/graphicprojects", { user });
 });
 
 router.get('/services', checkLoginStatus, async (req, res) => {
   const userId = req.user;
   const user = await User.findById(userId);
 
-  res.render("./home/services", { user });
+  res.render("/home/services", { user });
 });
 
 router.get('/partners', checkLoginStatus, async (req, res) => {
   const userId = req.user;
   const user = await User.findById(userId);
 
-  res.render("./home/partner", { user });
+  res.render("/home/partner", { user });
 });
 
 router.get('/about', checkLoginStatus, async (req, res) => {
   const userId = req.user;
   const user = await User.findById(userId);
 
-  res.render("./home/about", { user });
+  res.render("/home/about", { user });
 });
 
 router.get('/team', checkLoginStatus, async (req, res) => {
   const userId = req.user;
   const user = await User.findById(userId);
 
-  res.render("./home/team", { user });
+  res.render("/home/team", { user });
 });
 
 
