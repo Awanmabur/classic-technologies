35f369b16fa663ea8d81b58b2df05ca8cbe125d5
Awan mabur <82402071+Awanmabur@users.noreply.github.com>
2024-09-22 09:46:00 +0300

    compression gone

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const session = require('cookie-session');
 const flash = require('connect-flash');
 const { SitemapStream, streamToPromise } = require('sitemap');
 const { createGzip } = require('zlib');
-const compression = require('compression');
+// const compression = require('compression');
+// const helmet = require('helmet');
 
 
 const app = express();
@@ -23,7 +24,9 @@ connectMonggose();
 
 app.use(express.json());
 
-app.use(compression());
+// app.use(compression());
+//
+// app.use(helmet());
 
 //run seeders
 const {superAdmin} = require('./seeders/admin');
